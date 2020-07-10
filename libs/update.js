49b45f7c0ce63c032a9e0d766103d63645ee8a11
qcfaner <qcfaner@outlook.com>
2020-07-10 17:28:50 +0800

    'delete-example'

--- libs/update.js
+++ libs/update.js
@@ -14,8 +14,8 @@ module.exports = async (v) => {
       : true
     return node.lts && cp
   }).map(it => {
-    // 除去 files, openssl, uv, zlib 这些字段，其他的全部返回
-    const { files, openssl, uv, zlib, ...rest } = it
+    // 除去 files, openssl, uv, zlib, v8 这些字段，其他的全部返回
+    const { files, openssl, uv, zlib, v8, ...rest } = it
     return { ...rest }
   })
 }
